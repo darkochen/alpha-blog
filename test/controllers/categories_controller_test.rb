b23f785e504996cadde9dafcb28679135d023d25
Darko <darko.chen@taroko.io>
2019-04-30 15:20:37 +0800

    add restrict to admin can add categoties only

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class CategoriesControllerTest < ActionDispatch::IntegrationTest
   def setup
     @category = Category.create(name: "sports")
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
   end
   
   test  "should get categories index" do
@@ -19,5 +20,13 @@ class CategoriesControllerTest < ActionDispatch::IntegrationTest
     get category_path(@category)
     assert_response :success
   end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post categories_path, params: { category: { name: "sports" } }
+    end
+    assert_redirected_to categories_path
+  end
+
 end
     
