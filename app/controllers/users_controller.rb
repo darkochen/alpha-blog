6ffeaf94cbc2fbe7c465543e600c28077d20d021
Darko <darko.chen@taroko.io>
2019-04-30 11:09:11 +0800

    add admin action for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destory]
+  before_action :require_admin, only: [:destory]
+
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
 
   def new
     @user = User.new
@@ -33,8 +38,11 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
-  def index
-    @users = User.paginate(page: params[:page], per_page: 5)
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    flash[:danger] = "User and all articles created by user have been deleted"
+    redirect_to users_path
   end
   
   private
@@ -47,8 +55,15 @@ class UsersController < ApplicationController
   end
 
   def require_same_user
-    if current_user != @user
+    if current_user != @user and !current_user.admin?
       flash[:danger] = "You can only edit your own account"
+      redirect_to root_path
+    end
+  end
+
+  def require_admin
+    if logged_in? and !current_user.admin?
+      flash[:danger] = "Only admin users can perform that action"
       redirect_to root_path
     end
   end
